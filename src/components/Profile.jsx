e19de0797b173e10a8ab6d555018eebc191ffc36
beeplovsharma <beeplovsharma@gmail.com>
2024-06-04 13:51:29 +0530

    font fix

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -7,7 +7,7 @@ function Profile() {
     <>
       <div className="w-full min-h-screen bg-[#e8e8ed]">
         <div className="max-w-4xl mx-auto p-4">
-          <div className='text-center font-["luckiest_guy"] text-4xl md:text-5xl lg:text-6xl font-extrabold mb-6'>
+          <div className='text-center lucky text-4xl md:text-5xl lg:text-6xl font-extrabold mb-6'>
             <h1 className="bg-clip-text text-transparent bg-[url('https://i.pinimg.com/564x/e7/dc/aa/e7dcaa5680915af54485af4386f38207.jpg')]">
               memoirverse !
             </h1>
